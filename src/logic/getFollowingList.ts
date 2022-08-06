7eb337273e95e47e01658dd2edd30f26836e65c1
Muhammed Rajab <muhammedrajab101@gmail.com>
2022-08-06 19:37:23 +0530

    Successfully refactored the getFollowersList function

--- src/logic/getFollowingList.ts
+++ src/logic/getFollowingList.ts
@@ -1,6 +1,8 @@
 import fs from "fs";
 import { IgBot } from "../core/IgBot.js";
+import displayUserData from "./displayUserData.js";
 import { CommandLineUI } from "../cli/CommandLine.js";
+import storeUserDataAsJson from "./storeUserDataAsJson.js";
 
 export default async function getFollowingList(
     bot: IgBot,
@@ -19,7 +21,6 @@ export default async function getFollowingList(
         );
 
         const followingList = await bot.getFollowingList();
-
         CommandLineUI.success(
             `Successfully fetched users's following list`,
             "\n",
@@ -28,61 +29,45 @@ export default async function getFollowingList(
 
         // Ask the user whether to display the list of following
         CommandLineUI.log("");
-        if (
-            await CommandLineUI.confirm(
-                "Do you want to display the list of users who you are following?",
-            )
-        ) {
-            CommandLineUI.info(
-                `Here's the list of users that you are following ⤵️`,
-                "\n",
-                "\n",
-            );
-
-            CommandLineUI.success(
-                `# of followings: ${followingList.length}`,
-                "\n",
-                "",
-                false,
-            );
-
-            CommandLineUI.error(followingList.join("  "), "", "\n", false);
-        }
+        await displayUserData(
+            "Do you want to display the list of users who you are following?",
+            [
+                () =>
+                    CommandLineUI.info(
+                        `Here's the list of users that you are following ⤵️`,
+                        "\n",
+                        "\n",
+                    ),
+                () =>
+                    CommandLineUI.success(
+                        `# of followings: ${followingList.length}`,
+                        "\n",
+                        "",
+                        false,
+                    ),
+                () =>
+                    CommandLineUI.error(
+                        followingList.join("  "),
+                        "",
+                        "\n",
+                        false,
+                    ),
+            ],
+        );
 
         // Ask the user whether to save the list of following
         // Ask whether the user wants to store the list of users who are following as json
         CommandLineUI.log("");
-        if (
-            await CommandLineUI.confirm(
-                "Do you want to store the list of users who you are following?",
-            )
-        ) {
-            // Storing message
-            CommandLineUI.info(
-                `Storing the list of users who don't follow you back as json`,
-                "\n",
-                "\n",
-            );
+        await storeUserDataAsJson(
+            "list of users who you are following",
+            {
+                following_count: followingList.length,
+                following_list: followingList,
+            },
+            BOT_DATA_BASE_PATH,
+            "following_list",
+        );
 
-            // Storing the list of users who don't follow you back
-            const TIMESTAMP = Date.now();
-            const FILE_NAME = `${BOT_DATA_BASE_PATH}/following_list_${TIMESTAMP}.json`;
-            await fs.promises.writeFile(
-                FILE_NAME,
-                JSON.stringify({
-                    timestamp: Date.now(),
-                    following_count: followingList.length,
-                    following_list: followingList,
-                }),
-            );
-
-            // Storing was successful message
-            CommandLineUI.success(
-                `Successfully stored the list of users who you are following as json.\nCheckout ${FILE_NAME}`,
-                "\n",
-                "\n",
-            );
-        }
         if (loggingEnabled) CommandLineUI.displayLoggingEndMessage();
     } catch (err) {
         throw new Error(
